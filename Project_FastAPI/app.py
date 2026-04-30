406ac35c1e671cf7a3540554a6c29a8e6efba87d
SRINITHYA T <139114296+SRINITHYA04@users.noreply.github.com>
2026-04-30 18:11:26 +0000

    Learnt path and query parameters

--- Project_FastAPI/app.py
+++ Project_FastAPI/app.py
@@ -9,3 +9,14 @@ def root():
 @app.get("/Sri")
 def root():
     return{"messege":"Hello Srinithya"}
+
+# sending single parameter
+@app.get("/users/{user_id}")
+def user_name(user_id:int):
+    return{"user_id":user_id, "type":str(type(user_id))}
+
+# sending multiple parameter
+
+@app.get("/search")
+def search(q: str, page: int=1, limit: int=10):
+    return{"q":q, "page": page, "limit":limit}
